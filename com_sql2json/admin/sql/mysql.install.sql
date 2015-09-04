016f0afde43bf546734096c9e1918b269cad6e00
Viacheslav V Pryimak <vvpryimak@intropro.com>
2015-09-04 12:37:39 +0300

    sql type field changed

--- com_sql2json/admin/sql/mysql.install.sql
+++ com_sql2json/admin/sql/mysql.install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `#__sql2json` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`apiname` varchar(255) DEFAULT NULL,
-	`sqlaction` TINYTEXT DEFAULT NULL,
+	`sqlaction` TEXT DEFAULT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `apiname_UNIQUE` (`apiname`)
 );
